43c04b024f3e9405a3c00e686e7e6a8287141905
Rahul Kumar <138843710+rahulkumar40@users.noreply.github.com>
2025-09-07 16:47:50 +0530

    Delete 01-vivek-website directory
    i don' t want to show all

--- 01-vivek-website/src/assets/bgImg.js
+++
@@ -1,5 +0,0 @@
-import home3 from './hom3.jpg';
-import home1 from './home1.jpg';
-import home2 from './home2.jpg';
-const bgImg = [home1, home2, home3];
-export default bgImg
--- 01-vivek-website/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { AppSlice } from "./AppSlice";
-
-const store = configureStore({
-    reducer:AppSlice.reducer
-})
-
-export default store;
